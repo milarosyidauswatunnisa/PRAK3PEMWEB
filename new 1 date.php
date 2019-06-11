23c2392c90576baf18e27ab4b674110f532386ac
milarosyidauswatunnisa <48146032+milarosyidauswatunnisa@users.noreply.github.com>
2019-06-11 21:57:33 +0700

    if else, while, do while, switch, foreach

---
+++ new 1 date.php
@@ -0,0 +1,54 @@
+<?php
+/*$hari = date("d");
+var_dump($hari);
+if($hari<"10") {
+	echo "awal bulan";
+}
+else if ($hari > "20"){
+	echo "akhir bulan";
+}
+else {
+	echo "pertengahan bulan";
+}
+
+$meletus = "hujau";
+
+switch ($meletus) {
+	case "hijau":
+		echo " Dor";
+		break;	
+	case "kuning" :
+		echo "Dur";
+		break;
+	case "kelabu" :
+		echo "der";
+		break;
+	default:
+	echo "Balonku belum meletus !";
+}
+
+$w = 99;
+while($w < 100) {
+	echo "anda kurang sehat senilai $w <br>";
+	$w++;
+}
+do {
+	echo "nilai yang lulus adalah $w <br>";
+	$w++;
+}while ($w <=100);*/
+
+/*$kelas = array("satu", "dua" , "tiga");
+$room = ["empat", "lima", "enam"];
+//var_dump($kelas);
+//echo $kelas[1];
+
+	//echo $kelas . "<br>" . $room[0];
+	
+	foreach($kelas as $k) {
+		echo $k . "<br>";
+	}*/
+	
+	$kelas = [["satu"], ["dua"], ["tiga"]];
+	//echo $kelas[1][0];
+	var_dump($kelas);
+?>
